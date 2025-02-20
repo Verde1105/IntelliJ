41280943a182b1225540c0d1d1795e6f5a394929
Verde <rim3.office@gmail.com>
2025-02-20 18:01:56 +0900

    별찍기 기본형태 추가

--- jeetarains/jetbrains0219/src/Number2.java
+++ jeetarains/jetbrains0219/src/Number2.java
@@ -1,50 +1,12 @@
-import java.util.Scanner;
-
-
 //이하 계산기
 public class Number2 {
   public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
 
-    System.out.println("계산기를 실행합니다.");
-    System.out.println("숫자를 입력해주세요");
-    int x = scan.nextInt();
-
-    int qed = x;
-    int qed2 = qed;
-
-    while (true) {
-      scan.nextLine();
-      System.out.println("부호를 선택해 주세요.");
-      System.out.println("+ , - , * , /");
-      String z = scan.nextLine();
-      System.out.println("숫자를 입력해주세요");
-      int y = scan.nextInt();
-
-      switch (z) {
-        case "+":
-          qed += y;
-          System.out.println("= " + qed);
-          break;
-        case "-":
-          qed -= y;
-          System.out.println("= " + qed);
-          break;
-        case "*":
-          qed *= y;
-          System.out.println("= " + qed);
-          break;
-        case "/":
-          qed /= y;
-          System.out.println("= " + qed);
-          System.out.print("나머지는");
-          System.out.print(qed2 % y);
-          System.out.print("입니다.");
-          break;
-        case "=":
-          System.out.println("= " + qed);
-          return;
+    for (int i = 0; i <= 9; i++) {
+      for (int j = 1; j <= i; j++) {
+        System.out.print("*");
       }
+      System.out.println();
     }
   }
 }
