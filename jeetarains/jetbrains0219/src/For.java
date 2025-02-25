65d7eef93a5b71accf966941b8907ad171a51089
Verde <rim3.office@gmail.com>
2025-02-25 11:40:46 +0900

    으잉

---
+++ jeetarains/jetbrains0219/src/For.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class For {
+
+  public static void main(String[] args) {
+
+    int[] arr = {2, 8, 4, 9, 3};
+    int temp = 0;
+//미니 알고리즘 테스트
+
+    for (int i = 0; i <= 3; i++) {
+      for (int j = i + 1; j <= 4; j++) {
+        if (arr[i] > arr[j]) {
+          temp = arr[i];
+          arr[i] = arr[j];
+          arr[j] = temp;
+        }
+      }
+//      System.out.println(Arrays.toString(arr));
+
+    }
+    System.out.println(Arrays.toString(arr));
+
+  }
+
+
+}
