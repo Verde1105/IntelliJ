793e5e7d3695d3643dfa8688dea9d368cbf53e60
Verde <rim3.office@gmail.com>
2025-02-25 14:52:23 +0900

    알고리즘 테스트 추가

--- jeetarains/jetbrains0219/src/For.java
+++ jeetarains/jetbrains0219/src/For.java
@@ -7,10 +7,9 @@ public class For {
     int[] arr = {2, 8, 4, 9, 3};
     int temp = 0;
 //미니 알고리즘 테스트
-
     for (int i = 0; i <= 3; i++) {
       for (int j = i + 1; j <= 4; j++) {
-        if (arr[i] > arr[j]) {
+        if (arr[i] < arr[j]) {
           temp = arr[i];
           arr[i] = arr[j];
           arr[j] = temp;
